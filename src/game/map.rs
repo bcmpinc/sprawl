657b4190d046c0fbb599200a3bdaecb0a02d2a2c
Bauke Conijn <bcmpinc@gmail.com>
2025-06-05 03:09:40 +0200

    Seems like shader pipeline is validated.

--- src/game/map.rs
+++ src/game/map.rs
@@ -1,15 +1,20 @@
 use std::borrow::Cow;
 
 use bevy::{
-    asset::{AssetLoader, RenderAssetUsages},
+    asset::{LoadState, RenderAssetUsages},
     image::{ImageLoaderSettings, ImageSampler},
     prelude::*,
     render::{
+        extract_resource::{ExtractResource, ExtractResourcePlugin},
         mesh::PrimitiveTopology,
-        render_resource::{AsBindGroup, BindGroupLayout, CachedComputePipelineId, ComputePipelineDescriptor, PipelineCache, ShaderRef},
+        render_asset::RenderAssets,
+        render_graph::Node,
+        render_resource::{AsBindGroup, BindGroup, BindGroupLayout, CachedComputePipelineId, ComputePassDescriptor, ComputePipelineDescriptor, PipelineCache, ShaderRef, TextureUsages},
         renderer::RenderDevice,
-        RenderApp,
-    },
+        storage::GpuShaderStorageBuffer,
+        texture::{FallbackImage, GpuImage},
+        Render, RenderApp, RenderSet
+    }
 };
 
 use crate::screens::Screen;
@@ -21,6 +26,7 @@ pub(super) struct MapPlugin;
 impl Plugin for MapPlugin {
     fn build(&self, app: &mut App) {
         app.register_type::<TilemapMaterial>();
+        app.add_plugins(ExtractResourcePlugin::<ShaderData>::default());
         app.add_plugins(MaterialPlugin::<TilemapMaterial>{
             prepass_enabled: false,
             shadows_enabled: false,
@@ -28,11 +34,13 @@ impl Plugin for MapPlugin {
         });
         app.add_systems(OnEnter(Screen::Gameplay), setup);
         app.add_systems(Update, update_tile);
+        app.add_systems(Update, configure_image);
     }
 
     fn finish(&self, app: &mut App) {
         if let Some(render_app) = app.get_sub_app_mut(RenderApp) {
             render_app.init_resource::<KernelPipeline>();
+            render_app.add_systems(Render, prepare_compute.in_set(RenderSet::Prepare));
             println!("KernelPipeline added");
         }
     }
@@ -76,6 +84,7 @@ fn setup(
         |settings: &mut ImageLoaderSettings| {
             // Use `nearest` image sampling to preserve pixel art style.
             settings.sampler = ImageSampler::nearest();
+            settings.is_srgb = false;
         },
     );
 
@@ -83,15 +92,11 @@ fn setup(
         Name::new("Tilemap"),
         Mesh3d(meshes.add(mesh)),
         MeshMaterial3d(materials.add(TilemapMaterial{
-            tiles: tiles.clone(),
+            tiles,
             hover_tile: Vec4::ZERO,
         })),
         Transform::IDENTITY,
     ));
-
-    let data = ShaderData {
-        tiles
-    };
 }
 
 fn update_tile(mouse: Res<MousePos>, mut materials: ResMut<Assets<TilemapMaterial>>) {
@@ -103,9 +108,7 @@ fn update_tile(mouse: Res<MousePos>, mut materials: ResMut<Assets<TilemapMateria
     }
 }
 
-
-
-#[derive(TypePath,AsBindGroup)]
+#[derive(TypePath,AsBindGroup,Resource,Clone,ExtractResource)]
 struct ShaderData {
     #[storage_texture(0)] tiles: Handle<Image>,
 }
@@ -140,5 +143,99 @@ impl FromWorld for KernelPipeline {
             bind_group_layout,
             pipeline,
         }
+    }
+}
+
+pub struct DispatchKernel;
+
+#[derive(Debug, Resource)]
+pub struct KernelBindGroup(pub BindGroup);
+
+
+
+fn configure_image(
+    mut commands: Commands,
+    mut images: ResMut<Assets<Image>>,
+    material: Res<Assets<TilemapMaterial>>,
+    asset_server: Res<AssetServer>,
+    shader_data: Option<Res<ShaderData>>,
+) {
+    if shader_data.is_some() {return}
+    let Some(material) = &material.iter().next() else {return};
+    let handle = material.1.tiles.id();
+
+    // Wait until asset is fully loaded
+    if matches!(asset_server.get_load_state(handle), Some(LoadState::Loaded)) {
+        if let Some(image) = images.get_mut(handle) {
+            // Add the STORAGE_BINDING flag
+            image.texture_descriptor.usage |= TextureUsages::STORAGE_BINDING;
+            println!("Changed storage flag!");
+
+            commands.insert_resource(ShaderData {
+                tiles: material.1.tiles.clone()
+            });
+        }
+    }
+}
+
+fn prepare_compute<'a>(
+    mut commands: Commands,
+    shader_data: Option<Res<ShaderData>>,
+    render_device: Res<RenderDevice>,
+    pipeline: Res<KernelPipeline>,
+    bind_group: Option<Res<KernelBindGroup>>,
+    mut param: (
+        Res<'a, RenderAssets<GpuImage>>,
+        Res<'a, FallbackImage>,
+        Res<'a, RenderAssets<GpuShaderStorageBuffer>>
+    ),
+) {
+    if bind_group.is_some() {return}
+
+    let Some(shader_data) = shader_data else {
+        println!("shader_data not yet available");
+        return
+    };
+    let prepared_result = shader_data.as_bind_group(
+        &pipeline.bind_group_layout,
+        &render_device,
+        &mut param,
+    );
+    if let Ok(prepared_numbers) = prepared_result {
+        commands.insert_resource(KernelBindGroup(prepared_numbers.bind_group));
+        println!("bind success!");
+    } else {
+        println!("retry next update");
+        // we are retrying every frame regardless
+    }
+}
+
+impl Node for DispatchKernel {
+    fn run(
+        &self,
+        _graph: &mut bevy::render::render_graph::RenderGraphContext,
+        render_context: &mut bevy::render::renderer::RenderContext,
+        world: &bevy::prelude::World,
+    ) -> Result<(), bevy::render::render_graph::NodeRunError> {
+        // can't use because there is no view entity, uncommenting this line causes a hard-to-diagnose panic
+        //let _view_entity = graph.view_entity();
+        let kernel_pipeline = world.get_resource::<KernelPipeline>();
+        let kernel_bind_group = world.get_resource::<KernelBindGroup>();
+        let pipeline_cache = world.get_resource::<PipelineCache>();
+        if let (Some(kernel_pipeline), Some(kernel_bind_group), Some(pipeline_cache)) = (kernel_pipeline, kernel_bind_group, pipeline_cache) {
+            let mut pass = render_context
+                .command_encoder()
+                .begin_compute_pass(&ComputePassDescriptor {
+                    label: Some("Kernel Compute Pass"),
+                    timestamp_writes: None,
+                });
+            if let Some(real_pipeline) = pipeline_cache.get_compute_pipeline(kernel_pipeline.pipeline) {
+                println!("dispatch happening");
+                pass.set_pipeline(&real_pipeline);
+                pass.set_bind_group(0, &kernel_bind_group.0, &[]);
+                pass.dispatch_workgroups(1, 1, 1);
+            }
+        }
+        Ok(())
     }
 }
